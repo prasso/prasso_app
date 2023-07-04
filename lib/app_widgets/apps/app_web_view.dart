e03998dbf76b449a30e35222184c4854c613532c
Bobbi <bcp@faxt.com>
2023-07-04 08:20:42 -0400

    webview useragent set for oauth

--- lib/app_widgets/apps/app_web_view.dart
+++ lib/app_widgets/apps/app_web_view.dart
@@ -73,6 +73,8 @@ class AppRunWebView extends StatelessWidget {
             ]*/
         ),
         body: WebView(
+          userAgent:
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
           initialUrl: selectedUrl,
           javascriptMode: JavascriptMode.unrestricted,
           onWebViewCreated: (webViewController) {
